cbfd24d10c183c6f319661e83ebd4bcc33eb0273
lunaagustin <lunaagustin727@gmail.com>
2024-12-05 18:25:21 -0300

    Subir para revisar el codigo

--- TragamonedasModerno.ts
+++ TragamonedasModerno.ts
@@ -1,5 +1,54 @@
-//Hereda de tragamonedas 
+import { Resultado } from "./Resultado";
+import { Tragamonedas } from "./Tragamonedas";
 
-export class TragamonedasModerno{
+export class TragamonedasModerno extends Tragamonedas{
+    private tieneBonus: boolean;
+    private jackpot: number;
 
+    constructor(nombre:string,minApuesta:number,tematica:string){
+        super(nombre,minApuesta,tematica)
+        this.tieneBonus= this.setBonus();
+        this.jackpot= this.setJackpot();
+    }
+
+    public getBonus():boolean{
+        return this.tieneBonus;
+    }
+
+    public getJackpot():number{
+        return this.jackpot;
+    }
+
+    private setBonus():boolean{
+        return this.tieneBonus= true;
+    }
+
+    private setJackpot():number{
+        return this.jackpot= this.valorMultiplicador*5;
+    }
+
+    private setNumeroLineas():void{
+        this.numeroLineas.push("🚗","🚌","🚑","🚒","🚜");
+    }
+
+    public vehiculosAleatorias():void{
+        this.numeroLineas[Math.floor(Math.random()*3)];
+    }
+
+    // public jugar(monto: number): Resultado {
+    //     if(!this.realizarApuesta(monto)){
+    //         throw new Error("Error");
+    //     }
+
+    //     let vehiculo1= this.vehiculosAleatorias()
+    //     let vehiculo2= this.vehiculosAleatorias()
+    //     let vehiculo3= this.vehiculosAleatorias()
+
+    //     if(vehiculo1==vehiculo2 && vehiculo2==vehiculo3){
+    //         let ganancia= monto * this.jackpot;
+    //         return new Resultado(ganancia,"Ganaste");
+    //     }else{
+    //         return new Resultado(0,`Perdiste: ${monto}`);
+    //     }
+    // }
 }
